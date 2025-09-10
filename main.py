2e65ac69a1123cf9b07c7a9746634a3280b77f26
Barkro48 <tt02022561@gmail.com>
2025-09-10 10:14:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-
-def add_numbers(a, b):
-    return a + b
-from myfunc import add_numbers
-
-def main():
-    a = int(input("กรุณากรอกค่า A: "))
-    b = int(input("กรุณากรอกค่า B: "))
-
-    result = add_numbers(a, b)
-
-    print("ผลลัพธ์ของ A + B คือ:", result)
-
-if __name__ == "__main__":
-    main()
+from myfunc import add
+A = 10
+B = 5
+result = add(A, B)
+print(f"ผลลัพธ์ของ {A} + {B} คือ {result}")
